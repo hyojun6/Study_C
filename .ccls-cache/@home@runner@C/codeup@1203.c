dc72e6126dddbb939b699ca37360c1e7acd2a023
hyojun6 <162467802+hyojun6@users.noreply.github.com>
2024-08-06 19:00:31 +0000

    codeup

---
+++ .ccls-cache/@home@runner@C/codeup@1203.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(void) {
+  int a;
+  scanf("%d", &a);
+  if (a == 11 || a == 12 || a == 13) {
+    printf("%dth", a);
+  } else if (a % 10 == 1) {
+    printf("%dst", a);
+  } else if (a % 10 == 2) {
+    printf("%dnd", a);
+  } else if (a % 10 == 3) {
+    printf("%drd", a);
+  }
+}
