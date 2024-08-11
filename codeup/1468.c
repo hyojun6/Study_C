6557fec32845928eb272daa71b04a4d4e760cac4
hyojun6 <162467802+hyojun6@users.noreply.github.com>
2024-08-11 11:54:00 +0000

    codeup

---
+++ codeup/1468.c
@@ -0,0 +1,20 @@
+shell#include <stdio.h>
+
+int main(void) {
+  int a;
+  scanf("%d", &a);
+  int num = 1;
+  for (int i = 1; i <= a; i++) {
+
+    for (int j = a - 1; j >= 0; j--) {
+      if (i % 2 == 0) {
+        printf("%d ", 1 + a * (i - 1) + j);
+
+      } else {
+        printf("%d ", num);
+      }
+      num++;
+    }
+    printf("\n");
+  }
+}
