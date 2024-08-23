d38f36d2e839bbb47a3e3ef32656cc1e4929b2ff
hyojun6 <162467802+hyojun6@users.noreply.github.com>
2024-08-23 02:57:02 +0000

    codeup

--- codeup/1277.c
+++ codeup/1277.c
@@ -1,26 +1,25 @@
 #include <stdio.h>
+
 int main(void) {
-  int n, num, a;
-  int f,m,e;
-  scanf("%d", &n);
-  a=(n+1)/2;
-  for(int i=1; i<=n; i++) {
-    scanf("%d", &num);
-    switch (i) {
-    case 1:
-      f=num;
-      break;
-    case a:
-      m=num;
-      break;
-    case n:
-      e=num;
-      break;
+    int n, num;
+    int f = 0, m = 0, e = 0;
+    int a;
+
+    scanf("%d", &n);
+
+    a = (n + 1) / 2;
+
+    for (int i = 1; i <= n; i++) {
+        scanf("%d", &num);
+        if (i == 1) {
+            f = num;
+        } if (i == a) {
+            m = num;
+        } if (i == n) {
+            e = num;
+        }
     }
-    default:
-    num=num;
-    break;
-  }
-  printf("%d %d %d", f, m, e);
-  return 0;
+    printf("%d %d %d\n", f, m, e);
+
+    return 0;
 }
