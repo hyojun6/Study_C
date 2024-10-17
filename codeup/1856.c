fd04fa81faaee5589b006a456a789bea2249cd0e
hyojun6 <162467802+hyojun6@users.noreply.github.com>
2024-10-17 06:21:11 +0000

    codeup

---
+++ codeup/1856.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+long long int memo[26];
+
+long long int f(int n) {
+  if(memo[n])
+    return memo[n];
+  if(n==1) return 1;
+  if(n==2) return 2;
+  if(n==3) return 4;
+  return memo[n] = f(n-1) + f(n-2) + f(n-3);
+}
+
+int main(void) {
+  scanf("%lld", &memo[0]);
+  printf("%lld", f(memo[0]));
+  return 0;
+}
