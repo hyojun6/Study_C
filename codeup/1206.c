3cecd70eed3b14b8777c029d2c2c6dd7034399c8
hyojun6 <162467802+hyojun6@users.noreply.github.com>
2024-10-22 14:56:39 +0000

    codeup

--- codeup/1206.c
+++ codeup/1206.c
@@ -3,18 +3,12 @@
 int main(void) {
   int a, b, c;
   scanf("%d %d", &a, &c);
-  if (c % a == 0) {
+  if (c % a == 0) 
     printf("%d*%d=%d", a, c / a, c);
-    return 0;
-  }
-  if (a % c == 0) {
+  else if (a % c == 0)
     printf("%d*%d=%d", c, a / c, a);
-    return 0;
-  }
-  printf("none");
+  else
+    printf("none");
   //printf((c % a == 0) ? "%d*%d=%d", a, c / a, c : (a % c == 0) ? "%d*%d=%d", c, a / c, a : "none");
-
-  
-
   return 0;
 }
