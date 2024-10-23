ca3f65e2baf5ce383aac96f43648e1d8052ce044
hyojun6 <162467802+hyojun6@users.noreply.github.com>
2024-10-23 13:58:50 +0000

    codeup

--- codeup/1210.c
+++ codeup/1210.c
@@ -3,8 +3,7 @@
 int main(void) {
   int n[3], sum=0;
   scanf("%d %d", &n[1], &n[2]);
-  for(int i=1; i<=2; i++) {
+  for(int i=1; i<=2; i++)
     sum += (n[i]==1 ? 400 : n[i]==2 ? 340 : n[i] == 3 ? 170 : n[i]==4 ? 100 : 70);
-  }
   printf(sum>500 ? "angry" : "no angry"); 
 }
