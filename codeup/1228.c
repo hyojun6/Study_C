fc760b95413bf147192e9d939ee5939845991568
hyojun6 <162467802+hyojun6@users.noreply.github.com>
2024-10-27 12:38:22 +0000

    codeup

--- codeup/1228.c
+++ codeup/1228.c
@@ -3,15 +3,16 @@
 int main(void) {
   double k, m;
   scanf("%lf %lf", &k, &m);
-
   double avm = (k - 100) * 0.9;
   double result = (m - avm) * 100 / avm;
 
-  if (result > 20)
-    printf("비만\n");
-  else if (result > 10)
-    printf("과체중\n");
-  else
-    printf("정상\n");
+  // if (result > 20)
+  //   printf("비만\n");
+  // else if (result > 10)
+  //   printf("과체중\n");
+  // else
+  //   printf("정상\n");
+  printf((result > 20) ? "비만\n" : (result > 10) ? "과체중\n" : "정상\n");
+  
   return 0;
 }
