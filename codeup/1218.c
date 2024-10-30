85f8da6eddbd7ab95d7643f510df35d8e0788414
hyojun6 <162467802+hyojun6@users.noreply.github.com>
2024-10-30 14:57:48 +0000

    codeup

--- codeup/1218.c
+++ codeup/1218.c
@@ -5,19 +5,17 @@ int main(void) {
   scanf("%d %d %d", &a, &b, &c);
 
   if(a+b>c) {
-    if(a==b && b==c) {
+    if(a==b && b==c)
       printf("정삼각형");
-    } else if(a==b || b==c || c==a) {
+    else if(a==b || b==c || c==a)
       printf("이등변삼각형");
-    } else if(a*a+b*b==c*c) {
+    else if(a*a+b*b==c*c)
       printf("직각삼각형");
-    } else if(a+b>c){
+    else if(a+b>c)
       printf("삼각형");
-    } else {
+    else
       printf("삼각형");
-    }
-  } else {
+  } else
     printf("삼각형아님");
-  }
   return 0;
 }
