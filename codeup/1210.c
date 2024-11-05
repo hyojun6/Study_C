ec76bd57bc120aeb395666ae1e62037ff7dec44c
hyojun6 <162467802+hyojun6@users.noreply.github.com>
2024-11-05 15:04:48 +0000

    codeup

--- codeup/1210.c
+++ codeup/1210.c
@@ -6,4 +6,5 @@ int main(void) {
   for(int i=1; i<=2; i++)
     sum += (n[i]==1 ? 400 : n[i]==2 ? 340 : n[i] == 3 ? 170 : n[i]==4 ? 100 : 70);
   printf(sum>500 ? "angry" : "no angry"); 
+  return 0;
 }
