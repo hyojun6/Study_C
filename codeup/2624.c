26cb08cec4b92a3f03290b916a8534bca2cc861e
hyojun6 <162467802+hyojun6@users.noreply.github.com>
2024-11-08 12:08:17 +0000

    codeup

--- codeup/2624.c
+++ codeup/2624.c
@@ -22,14 +22,11 @@ int main() {
   int n, i;
   scanf("%d", &n);
 
-  for (i = 1; i <= n; i++) {
+  for (i = 1; i <= n; i++)
     f(i);
-  }
   // printf("%d", sum);
   printf("-------------\n");
-  for (i = 0; i < n - 1; i++) {
+  for (i = 0; i < n - 1; i++)
     printf("%d\n", result[i]);
-  }
-
   return 0;
 }
