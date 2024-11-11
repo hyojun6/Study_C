a344dc97622418c4f4407e402357ecbe9ca68b75
hyojun6 <162467802+hyojun6@users.noreply.github.com>
2024-11-11 13:50:22 +0000

    codeup

--- codeup/1925.c
+++ codeup/1925.c
@@ -12,3 +12,21 @@ int main(void) {
   printf("%d", f(n, r));
   return 0;
 }
+
+/*
+#include <stdio.h>
+
+int comb(int n, int r) {
+    if(r==0 || n==r)
+        return 1;
+    else
+        return comb(n-1, r-1)+comb(n-1, r);
+}
+
+int main(void) {
+    int n, r;
+    scanf("%d %d", &n, &r);
+    printf("%d", comb(n, r));
+}
+
+*/
